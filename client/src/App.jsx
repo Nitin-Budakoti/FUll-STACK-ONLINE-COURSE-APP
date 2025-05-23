d628f0acc817a7ffd7c1d2685be8e5a860048296
Nitin Budakoti <manuboda8885@gmail.com>
2025-05-23 09:45:19 +0530

    navbar-section-added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
 import Login from "./Pages/Login"
+import  Navbar  from "./components/Navbar.jsx"
 
 function App() {
   return (
     <main>
+    <Navbar />
        <Login />
     </main>
   )
